9dd469edc55f9727730e9d0d549b9cb63f850997
Ampumuza Recheal <ampumuzarecheal152@gmail.com>
2025-01-07 15:58:35 +0300

    Add files via upload

---
+++ TriathlonSytem/src/main/java/vu25/triathlonsytem/TriathlonSytem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package vu25.triathlonsytem;
+
+/**
+ *
+ * @author drggf
+ */
+public class TriathlonSytem {
+
+    public static void main(String[] args) {
+        System.out.println("TriathlonSytem");
+    }
+}
